65e58082f40b8ea60e02f52acc17326de9400709
Japnoor Kaur <kaurjapnoor123@gmail.com>
2024-11-17 00:43:14 +0530

    Update Keccak.py

--- CryptographicAlgorithms/HashFunctions/Keccak.py
+++ CryptographicAlgorithms/HashFunctions/Keccak.py
@@ -1,22 +1,24 @@
-''' 
+'''
 Created on 15th November by Japnoor Kaur and Mahee Agarwal.
 
-This module implements the Keccak hash function.
-''' 
+This module implements the Keccak hash function and outputs results in a JSON file.
+'''
 
+import json
 from Crypto.Hash import SHA3_256
 
-def keccak_hash_from_inputs(digest_bits=256, update_after_digest=False):
+def keccak_hash_to_json_file(digest_bits=256, update_after_digest=False, output_filename="keccak_hash_output.json"):
     """
     Continuously takes input from the user, updates the Keccak hash object,
-    and computes the final digest when input stops.
+    computes the final digest when input stops, and saves the result in a JSON file.
 
     Parameters:
         digest_bits (int): The size of the digest in bits (224, 256, 384, 512).
         update_after_digest (bool): Whether digest() can be followed by update().
+        output_filename (str): The name of the output JSON file.
 
     Returns:
-        str: Hexadecimal digest of the accumulated input.
+        str: Path to the JSON file containing the hash output.
     """
     # Define valid digest sizes and their corresponding byte sizes
     valid_digests = {224: 28, 256: 32, 384: 48, 512: 64}
@@ -49,6 +51,18 @@ def keccak_hash_from_inputs(digest_bits=256, update_after_digest=False):
                 break
             keccak_obj.update(user_input.encode('utf-8'))
 
-    # Return the hexadecimal digest of the accumulated input
-    return keccak_obj.hexdigest()
+    # Compute the hexadecimal digest of the accumulated input
+    keccak_digest = keccak_obj.hexdigest()
 
+    # Create a dictionary for the JSON output
+    hash_data = {
+        "algorithm": f"Keccak-{digest_bits}",
+        "hexdigest": keccak_digest
+    }
+
+    # Write the dictionary to a JSON file
+    with open(output_filenamekeccak, "w") as json_file:
+        json.dump(hash_data, json_file, indent=4)
+
+    # Return the path to the JSON file
+    return output_filenamekeccak
